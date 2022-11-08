1090754c3ba2a8d647be7a57ce7fedcc0ad2f05e
Grant Whitney <89329368+grantwhitney1@users.noreply.github.com>
2022-11-08 04:50:48 -0600

    Window Init

--- C--Game-Engine/C--Game-Engine/Program.cs
+++ C--Game-Engine/C--Game-Engine/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using Window;
+
+namespace Engine
+{
+    public class Program
+    {
+        const double MAXFR = 60.0;
+
+        public static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.RenderFrequency = MAXFR;
+                game.UpdateFrequency = MAXFR;
+
+                game.Run();
+            }
+        }
+    }
+}
+
+namespace Window
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title)
+            : base(GameWindowSettings.Default,
+                  new NativeWindowSettings()
+                  {
+                      Size = (width, height),
+                      Title = title
+                  })
+        { }
+    }
+}
